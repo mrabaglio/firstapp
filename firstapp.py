5b2918d582558dd0d1b5310022fa3bad193d74f1
mrabaglio <mrabaglio@gmail.com>
2023-01-09 14:52:47 -0300

    Update firstapp.py

--- firstapp.py
+++ firstapp.py
@@ -9,8 +9,8 @@ import pandas as pd
 
 # Sin usar st.write
 df = pd.DataFrame({
-  'first column': [1, 2, 3, 4],
-  'second column': [10, 20, 30, 40]
+  'column 1': [1, 2, 3, 4],
+  'column 2': [10, 20, 30, 40]
 })
 
 df
